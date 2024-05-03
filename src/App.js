479cbe4e269617a0f322dbd7452b0b5bc5d0552a
Mikkel Lau <mikkellau98@hotmail.com>
2024-05-03 14:20:53 +0200

    ghosthunting wp containerized react frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import './style/App.css';
+ 
+// Define a React functional component named App
 function App() {
+  // Initialize state 'posts' with an empty array. This state will hold the fetched posts.
+  const [posts, setPosts] = useState([])
+  const [pages, setPages] = useState([])
+
+ 
+  // useEffect hook to fetch posts from the WordPress REST API when the component mounts
+  useEffect(()=>{
+      // Use axios to make a GET request to the WordPress REST API for posts
+      axios.get('http://localhost:8080/wp-json/wp/v2/posts?_embed')
+      .then((res)=>setPosts(res.data)) // On success, update the 'posts' state with the fetched data
+  },[]) // The empty dependency array means this effect runs only once after the initial render
+ 
+  useEffect(()=>{
+      // Use axios to make a GET request to the WordPress REST API for posts
+      axios.get('http://localhost:8080/wp-json/wp/v2/pages?_embed')
+      .then((res)=>setPages(res.data)) // On success, update the 'posts' state with the fetched data
+  },[]) // The empty dependency array means this effect runs only once after the initial render
+ 
+  // Map each post in the 'posts' state to an <li> element with its content
+  // const postsJsx = posts.map((post)=>(
+  //     // Set the inner HTML of the <li> to the post's content. Note: This can be a security risk (XSS)
+  //     <li key={post.id} dangerouslySetInnerHTML={{__html:post.content.rendered}}></li> 
+
+  // ))
+
+
+
+// WooCommerce below
+useEffect(()=>{
+  // Use axios to make a GET request to the WordPress REST API for posts
+  axios.get('http://localhost:8080/wp-json/wc/v3/products')
+  .then((res)=>setPages(res.data)) // On success, update the 'posts' state with the fetched data
+},[]) // The empty dependency array means this effect runs only once after the initial render
+
+
+const WooCommerce = axios.create({
+  baseURL: 'http://localhost:8080/wp-json/wc/v3/products',
+  headers: {
+  Authorization: 'admin',
+  },
+  });
+
+
+
+  // Render the list of posts inside a <ul> element
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2 dangerouslySetInnerHTML={{ __html: post.title.rendered }}></h2>
+          <div dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
+            {/* <div dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
+            <div dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
+            <div dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div> */}
+        </div>
+      ))}
+    
+
+      {pages.map((page) => (
+        <div key={page.id}>
+          <h2 dangerouslySetInnerHTML={{ __html: page.title.rendered }}></h2>
+          <div dangerouslySetInnerHTML={{ __html: page.content.rendered }}></div>
+            {/* <div dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
+            <div dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
+            <div dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div> */}
+        </div>
+      ))}
     </div>
   );
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './style/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './style/input.css';
+
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+
+// const http = require('http');
+// const https = require('https');
+// const axios = require('axios');
+// require("dotenv").config();
+
+// const agent = new https.Agent({
+//   rejectUnauthorized: false
+// });
+
+// const PORT = process.env.PORT || 3000;
+
+// const app = http.createServer((req, res) => {
+//   if (req.url == "/products" && req.method == "GET") {
+//       // get the token
+//       let token = `${process.env.consumer_key}:${process.env.consumer_secret}`;
+//       // get the base 64 encoded string.
+//       let basic_auth = Buffer.from(token).toString('base64');
+//       // Send request to get the data.
+//       axios.default.get('http://localhost:8080/wp-json/wc/v3/products', {
+//           httpsAgent: agent,
+//           headers: {
+//               'Content-Type': 'application/json',
+//               'Authorization': 'Basic ' + basic_auth
+//           }
+//       })
+//           .then((response) => {
+//               // Successful response
+//               console.log("Successful request");
+//               res.statusCode = response.status;
+//               res.writeHead(200, { 'Content-Type': 'application/json' });
+//               res.end(JSON.stringify(response.data));
+//           })
+//           .catch((error) => {
+//               // Unsuccessful response
+//               console.log("Unsuccessful request");
+//               let message = new Error(error.message);
+//               res.statusCode = 500;
+//               res.end(JSON.stringify(message));
+//           })
+//   }
+// });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Navbar />
     <App />
+    <Footer />
   </React.StrictMode>
 );
 
